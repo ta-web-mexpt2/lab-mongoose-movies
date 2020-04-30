2a6eb58a99521afc069c195c411f1cda1879bd6f
Ben Balderas <benbalderas3@gmail.com>
2020-04-29 19:45:40 -0500

    iteration 9 done

--- starter-code/server/routes/moviesRoute.js
+++ starter-code/server/routes/moviesRoute.js
@@ -7,7 +7,16 @@ router.get('/', (req, res) => {
     Movie.find().populate('cast').then(movies => {
         res.status(200).json({ movies })
     })
-})
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Movie.findById(id, req.body).populate('cast').then(movie => {
+      res.status(200).json({ movie })
+  })
+  .catch(err => res.status(400).json({ err }))
+});
 
 // Create
 router.post('/create', (req, res) => {
@@ -15,6 +24,25 @@ router.post('/create', (req, res) => {
       res.status(201).json({ movie })
     })
     .catch(err => res.status(400).json({ err }))
+});
+
+// Update
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Movie.findByIdAndUpdate(id, req.body, { new: true }).then(movie => {
+    res.json({ movie })
+  })
+});
+
+// Delete
+router.delete('/:id/delete', (req, res) => {
+  const { id } = req.params;
+
+  Movie.findByIdAndRemove(id).then(movie => {
+    res.json({ movie })
+  })
+  .catch(err => res.status(400).json({ err }))
 });
 
   module.exports = router;
