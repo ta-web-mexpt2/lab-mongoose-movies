993a0f246c186efd3a5b6fe8fbc206cce8ccf4a5
Gala Torres <galatorresrovelo@gmail.com>
2020-11-21 09:51:14 -0600

    Lo que pude hacer :-(

--- starter-code/server/routes/index.js
+++ starter-code/server/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', (req, res, next) => {
   res.status(200).json({message:"estas listo para la accion"});
 });
 
+//Agregar Celebrity
+router.get('/celebrity/create', (req, res, next) => {
+  res.render("celebrity-create");
+});
+router.post('/celebrity/create', (req, res, next) => {
+  //Generadas las variables
+  const {name,occupation,catchPhrase} = req.body;
+  //La instancia del nuevo libro
+  const newBook = new Celebrity({name,occupation,catchPhrase});
+
 module.exports = router;
